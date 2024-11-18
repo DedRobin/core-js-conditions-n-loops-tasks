da11f918f5ceee8f5bfd2089a0796f8e0ec24cd4
Pavel Rabekin <pavelrabekin@gmail.com>
2024-11-18 10:47:19 +0300

    feat: isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -88,8 +88,11 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (!(a && b && c)) return false;
+  return (
+    (a === b && a + b > c) || (b === c && b + c > a) || (a === c && a + c > b)
+  );
 }
 
 /**
