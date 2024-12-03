719b4fd109d1c1c0863301e8b879bc9d590463c5
Pavel Rabekin <pavelrabekin@gmail.com>
2024-12-03 10:04:41 +0300

    feat: shuffleChar

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -481,8 +481,18 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let output = str;
+  for (let i = 0; i < iterations; i += 1) {
+    let odd = '';
+    let even = '';
+    for (let j = 0; j < output.length; j += 1) {
+      if (j % 2) odd += output[j];
+      else even += output[j];
+    }
+    output = even + odd;
+  }
+  return output;
 }
 
 /**
